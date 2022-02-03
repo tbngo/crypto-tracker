44c259f9a8e8a029df98f6578627b5bd8eff1a87
thengo1 <tbn2109@columbia.edu>
2022-02-03 06:53:29 -0500

    adding TailwindCSS

---
+++ src/components/Entry.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Entry() {
+  return (
+    <div>
+      This is an entry
+    </div>
+  );
+}
+
+export default Entry;
---
+++ src/components/Table.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Entry from "./Entry"
+
+function Table() {
+  return (
+    <div>
+      <Entry />
+    </div>
+  );
+}
+
+export default Table;
