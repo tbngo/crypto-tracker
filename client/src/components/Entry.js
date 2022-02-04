7a16497e4eb9ab35df2a222d0a8db27774758a16
thengo1 <tbn2109@columbia.edu>
2022-02-03 22:02:53 -0500

    file restructure and setting up API endpoints

---
+++ client/src/components/Entry.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Entry({ coin }) {
+  return (
+    <div className="w-2/5 h-12 border flex flex-row gap-x-3 items-center">
+      <div className="ml-4">
+        {coin}
+      </div>
+      <div>
+        $37,183.80
+      </div>
+      <div>
+        -3.64 (-1.27%)
+      </div>
+    </div>
+  );
+}
+
+export default Entry;
