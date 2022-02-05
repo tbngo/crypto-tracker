9125856048136cf2ce9ccd4bdd482b1dad7b687a
thengo1 <tbn2109@columbia.edu>
2022-02-05 03:33:11 -0500

    adding manual proxy and fetching data from relay route for table entries

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import Entry from "./Entry"
+import Entry from "./Entry";
 
 function Table() {
-  const [symbols, setSymbols] = useState([])
+  const [symbols, setSymbols] = useState(["BTC", "ETH", "DOGE"]);
 
   // TODO: add user profiles to allow deleting/adding different symbols
   useEffect(() => {
-    setSymbols(["BTC", "ETH", "DOGE"]);
+    const fetchData = async () => {
+      try {
+        const data = await fetch(`/api/coin?q=${symbols.join()}`)
+        console.log(data);
+      } catch (err) {
+        return console.error(err)
+      }
+    };
+    fetchData();
   }, [symbols]);
 
   const symbolEntries = symbols.map((symbol) => 
